08c9ba4f5fca569a0666870676db43a9ba74529f
takuto0831 <leeloon831@gmail.com>
2018-12-10 21:05:08 +0900

    first commit

--- script/function.R
+++ script/function.R
@@ -4,38 +4,44 @@
 # Plot_prophet: 実測値(点), 予測区間, 予測線 
 ################
 
-Prophet <- function(data,name){
+Prophet <- function(data,name,event){
   # model
   model <- data %>% 
     filter(park == name) %>% 
     select(datetime,visitors) %>% 
     rename(ds = datetime, y = visitors) %>% 
-    prophet(weekly.seasonality = TRUE,
-            yearly.seasonality = TRUE)
+    prophet(changepoint.prior.scale = 0.5,
+            weekly.seasonality = TRUE,
+            yearly.seasonality = TRUE,
+            daily.seasonality = FALSE,
+            holidays = event)
   # predict
   future <- make_future_dataframe(model,365)
   forecast <- predict(model,future) %>% 
     mutate(park = name) %>% 
     select(ds,park,trend,weekly,weekly_lower,weekly_upper,yearly,yearly_lower,
-           yearly_upper,yhat,yhat_lower,yhat_upper) 
+           yearly_upper,yhat,yhat_lower,yhat_upper,holidays) 
   return(forecast)
 }
-
-Log_prophet <- function(data,name){
+########## 要修正 #############
+Log_prophet <- function(data,name,event){
   # model
   model <- data %>% 
     filter(park == name) %>% 
     select(datetime,visitors) %>% 
     rename(ds = datetime, y = visitors) %>% 
     mutate(y = log1p(y)) %>% 
-    prophet(weekly.seasonality = TRUE,
-            yearly.seasonality = TRUE)
+    prophet(changepoint.prior.scale = 0.5,
+            weekly.seasonality = TRUE,
+            yearly.seasonality = TRUE,
+            daily.seasonality = FALSE,
+            holidays = event)
   # predict
   future <- make_future_dataframe(model,365)
   forecast <- predict(model,future) %>% 
     mutate(park = name) %>% 
     select(ds,park,trend,weekly,weekly_lower,weekly_upper,yearly,yearly_lower,
-           yearly_upper,yhat,yhat_lower,yhat_upper) %>% 
+           yearly_upper,yhat,yhat_lower,yhat_upper,holidays) %>% 
     mutate_if(is.numeric,expm1)
   # prophet_plot_components(model,forecast) + theme_classic(base_family = "HiraKakuPro-W3")
   return(forecast)
@@ -62,7 +68,12 @@ Plot_prophet_components <- function(forecast){
     ggplot(aes(x=ds,y=yearly,group=1)) +
     geom_line() +
     theme_classic(base_family = "HiraKakuPro-W3")
-  grid.arrange(p1,p2,p3,nrow=3)
+  # holidays
+  p4 <- forecast %>% 
+    ggplot(aes(x=ds,y=holidays)) +
+    geom_line() +
+    theme_classic(base_family = "HiraKakuPro-W3")
+  grid.arrange(p1,p2,p3,p4,nrow=4)
 }
 
 Plot_prophet_predict <- function(forecast,data){
@@ -73,15 +84,17 @@ Plot_prophet_predict <- function(forecast,data){
     geom_ribbon(aes(ymin=yhat_lower,ymax=yhat_upper),alpha=0.5) +
     geom_line(aes(y=yhat),color="blue") +
     geom_point(aes(y=visitors)) + 
-    ggtitle(forecast$park[1]) +
+    facet_zoom(x = ds >= "2015-08-01" & ds <= "2015-09-30") +
+    ggtitle(forecast$park[1]) + 
     theme_classic(base_family = "HiraKakuPro-W3")
 }
 
 MeanAbsoluteError <- function(forecast,data){
-  forecast %>% 
+  tmp <- forecast %>% 
     mutate(ds = as.Date(ds)) %>% 
     inner_join(data,by=c("ds"="datetime","park" = "park")) %>% 
     mutate(dif = abs(yhat-visitors)) %>% 
     summarise(mean(dif)) %>% 
-    return()
+    as.numeric() 
+  print(paste("公園名:",forecast$park[1],"MAE:",tmp))
 }
